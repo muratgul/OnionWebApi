64c34f6966f26f11674cee7078cd419bed9b1d48
YOLBULAN\bim03 <muratgul@gmail.com>
2025-07-24 15:06:20 +0300

    Add Scalar

---
+++ src/Presentation/UnionWebApi.Api/Controllers/BrandsController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UnionWebApi.Application.Features.Brands.Quaries;
+
+namespace UnionWebApi.Api.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class BrandsController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public BrandsController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> GetAll()
+    {
+        var response = await mediator.Send(new GetAllBrandsQueryRequest());
+        return Ok(response);
+    }
+}
--- src/Presentation/UnionWebApi.Api/Program.cs
+++ src/Presentation/UnionWebApi.Api/Program.cs
@@ -1,17 +1,55 @@
+using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Components ??= new();
+        document.Components.SecuritySchemes ??= new Dictionary<string, OpenApiSecurityScheme>();
+
+        document.Components.SecuritySchemes["Bearer"] = new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            Description = "JWT Authorization header using the Bearer scheme."
+        };
+
+        document.SecurityRequirements.Add(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+
+        return Task.CompletedTask;
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(option =>
+    {
+        option
+        .WithTitle("Union Web API")
+        .WithTheme(ScalarTheme.Default)
+        .WithDarkMode(false);
+    });
 }
 
 app.UseAuthorization();
