1d3d75d00e9febd6f8435686fdf87eb510c82187
YOLBULAN\bim03 <muratgul@gmail.com>
2025-07-29 17:29:07 +0300

    Add Brand query validator.

---
+++ src/Core/OnionWebApi.Application/Features/Brands/Quaries/GetBrandQueryValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace OnionWebApi.Application.Features.Brands.Quaries;
+public class GetBrandQueryValidator : AbstractValidator<GetBrandQueryRequest>
+{
+    public GetBrandQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id cannot be empty")
+            .GreaterThan(0).WithMessage("Id must be greater than 0");
+    }
+}
