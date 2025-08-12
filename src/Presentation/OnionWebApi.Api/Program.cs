6ffcb2c028c65e8f68dd0234c6650bcc639fd58b
YOLBULAN\bim03 <muratgul@gmail.com>
2025-08-12 08:22:44 +0300

    Refactoring

--- src/Presentation/OnionWebApi.Api/Program.cs
+++ src/Presentation/OnionWebApi.Api/Program.cs
@@ -1,8 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 var env = builder.Environment.EnvironmentName;
-var envFile = $".env.{env.ToLower()}";
+var envFile = File.Exists($".env.{env.ToLower()}") ? $".env.{env.ToLower()}" : ".env";
+
 DotEnv.Load(new DotEnvOptions(envFilePaths: new[] { envFile }, overwriteExistingVars: true));
+
+
 builder.Configuration.AddEnvironmentVariables();
 
 builder.RegisterServices(typeof(Program));
