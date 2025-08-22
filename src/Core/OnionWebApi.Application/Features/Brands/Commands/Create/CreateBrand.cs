bb38f7fb8eb3b3ac11c45198acd6b38e0d643fb4
YOLBULAN\bim03 <muratgul@gmail.com>
2025-08-22 14:24:27 +0300

    Refactor event codes

--- src/Core/OnionWebApi.Application/Features/Brands/Commands/Create/CreateBrand.cs
+++ src/Core/OnionWebApi.Application/Features/Brands/Commands/Create/CreateBrand.cs
@@ -7,11 +7,9 @@ public class CreateBrandCommandRequest : IRequest<Brand>
 internal class CreateBrandCommandHandler : BaseHandler, IRequestHandler<CreateBrandCommandRequest, Brand>
 {
     private readonly BrandRules _brandRules;
-    private readonly IMediator _mediator;
-    public CreateBrandCommandHandler(BrandRules brandRules, IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor, IUriService uriService, IRedisCacheService redisCacheService, IMediator mediator) : base(mapper, unitOfWork, httpContextAccessor, uriService, redisCacheService)
+    public CreateBrandCommandHandler(BrandRules brandRules, IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor, IUriService uriService, IRedisCacheService redisCacheService) : base(mapper, unitOfWork, httpContextAccessor, uriService, redisCacheService)
     {        
         _brandRules = brandRules;
-        _mediator = mediator;
     }
 
     public async Task<Brand> Handle(CreateBrandCommandRequest request, CancellationToken cancellationToken)
@@ -21,17 +19,10 @@ internal class CreateBrandCommandHandler : BaseHandler, IRequestHandler<CreateBr
         var brand = _mapper.Map<Brand>(request);
         brand.IsDeleted = false;
 
-        await _unitOfWork.GetWriteRepository<Brand>().AddAsync(brand);
-        await _unitOfWork.SaveAsync();
-
         brand.CreateBrand(request.Name);
 
-        foreach (var domainEvent in brand.DomainEvents)
-        {
-            await _mediator.Publish(domainEvent, cancellationToken);
-        }
-
-        brand.ClearDomainEvents();
+        await _unitOfWork.GetWriteRepository<Brand>().AddAsync(brand);
+        await _unitOfWork.SaveAsync();
 
         await _redisCacheService.RemoveAsync("GetAllBrands");
 
--- src/Infrastructure/OnionWebApi.Persistence/UnitOfWorks/UnitOfWork.cs
+++ src/Infrastructure/OnionWebApi.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -1,11 +1,29 @@
-﻿namespace OnionWebApi.Persistence.UnitOfWorks;
-public class UnitOfWork(AppDbContext dbContext) : IUnitOfWork
+﻿using MediatR;
+
+namespace OnionWebApi.Persistence.UnitOfWorks;
+public class UnitOfWork(AppDbContext dbContext, IMediator mediator) : IUnitOfWork
 {
+    private readonly IMediator mediator = mediator;
     private readonly AppDbContext dbContext = dbContext;
 
     public async ValueTask DisposeAsync() => await dbContext.DisposeAsync();
     public int Save() => dbContext.SaveChanges();
-    public async Task<int> SaveAsync() => await dbContext.SaveChangesAsync();
+    public async Task<int> SaveAsync()
+    {
+        var domainEntities = dbContext.ChangeTracker.Entries<BaseEntity>()
+            .Where(x => x.Entity.DomainEvents is not null && x.Entity.DomainEvents.Any());
+
+        var domainEvents = domainEntities.SelectMany(x => x.Entity.DomainEvents).ToList();
+        domainEntities.ToList().ForEach(entity => entity.Entity.ClearDomainEvents());
+        var result = await dbContext.SaveChangesAsync();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await mediator.Publish(domainEvent);
+        }
+
+        return result;
+    }
     ICustomRepository<T> IUnitOfWork.GetCustomRepository<T>() => new CustomRepository<T>(dbContext);
     IReadRepository<T> IUnitOfWork.GetReadRepository<T>() => new ReadRepository<T>(dbContext);
     IWriteRepository<T> IUnitOfWork.GetWriteRepository<T>() => new WriteRepository<T>(dbContext);
