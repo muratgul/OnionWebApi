732e53f072154ed220878535d385c52d6846dfb2
YOLBULAN\bim03 <muratgul@gmail.com>
2025-08-29 08:43:29 +0300

    Refactor

--- src/Presentation/OnionWebApi.Api/Extensions/HttpExtensions.cs
+++ src/Presentation/OnionWebApi.Api/Extensions/HttpExtensions.cs
@@ -8,7 +8,7 @@ public static class HttpExtensions
         {
             HttpOnly = true,
             Secure = true,
-            SameSite = SameSiteMode.Lax,
+            SameSite = SameSiteMode.Strict,
             Expires = expiryTime
         };
         response.Cookies.Append("refreshToken", refreshToken, cookieOptions);
