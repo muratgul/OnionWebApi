ecd2db06541d7324e44632fc4cf046c9544ccdcd
YOLBULAN\bim03 <muratgul@gmail.com>
2025-09-11 11:14:51 +0300

    Refactory

--- src/Core/OnionWebApi.Application/Beheviors/HybridCacheBehavior.cs
+++ src/Core/OnionWebApi.Application/Beheviors/HybridCacheBehavior.cs
@@ -1,16 +1,25 @@
-﻿namespace OnionWebApi.Application.Beheviors;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OnionWebApi.Application.Beheviors;
 public class HybridCacheBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
     private readonly ICacheService _cacheService;
+    private readonly IConfiguration _configuration;
 
-    public HybridCacheBehavior(ICacheService cacheService)
+    public HybridCacheBehavior(ICacheService cacheService, IConfiguration configuration)
     {
         _cacheService = cacheService;
+        _configuration = configuration;
     }
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        if(request is ICacheableQuery query)
+        var isCacheEnabled = _configuration.GetValue<bool>("CacheSettings:Enabled");
+        if (!isCacheEnabled)
+        {
+            return await next(cancellationToken);
+        }
+        if (request is ICacheableQuery query)
         {
             var cacheKey = query.CacheKey;
             var cacheTime = query.CacheTime;
@@ -23,15 +32,15 @@ public class HybridCacheBehavior<TRequest, TResponse> : IPipelineBehavior<TReque
 
             var cachedData = await _cacheService.GetAsync<TResponse>(cacheKey, cancellationToken);
 
-            if(cachedData is not null)
+            if (cachedData is not null)
             {
                 return cachedData;
             }
 
             var response = await next(cancellationToken);
 
-            if(response is not null)
-            {                
+            if (response is not null)
+            {
                 await _cacheService.SetAsync(cacheKey, response, TimeSpan.FromMinutes(cacheTime), tags, cancellationToken);
             }
 
