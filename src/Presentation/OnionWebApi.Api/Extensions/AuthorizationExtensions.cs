116a6240df8eb6665dc69a23703431a1561a7409
YOLBULAN\bim03 <muratgul@gmail.com>
2026-04-06 10:03:48 +0300

    Add AuthorizationExtensions

---
+++ src/Presentation/OnionWebApi.Api/Extensions/AuthorizationExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace OnionWebApi.Api.Extensions;
+
+public static class AuthorizationExtensions
+{
+    public static TBuilder RequireAnyPermission<TBuilder>(
+        this TBuilder builder, params string[] permissions)
+        where TBuilder : IEndpointConventionBuilder
+    {
+        return builder.RequireAuthorization(policy =>
+            policy.RequireAssertion(ctx =>
+                permissions.Any(p => ctx.User.HasClaim("Permission", p))));
+    }
+}
